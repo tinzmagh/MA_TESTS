540d919e21e5c960784034253e3e6818ed8a4bf9
Martine <martinemagh@gmail.com>
2020-04-02 13:24:17 +0200

    more tests

--- src/test/java/mandatory/test/PurchaseTest.java
+++ src/test/java/mandatory/test/PurchaseTest.java
@@ -3,16 +3,20 @@ package mandatory.test;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PurchaseTest {
 
+    ArrayList<String> cellPhones = new ArrayList<String>( Arrays.asList("Motorola G99", "iPhone 99", "Samsung Galaxy 99", "Sony Xperia 99", "Huawei 99"));
+    ArrayList<Integer> cellPhonePrices = new ArrayList<Integer>( Arrays.asList(800, 6000, 1000, 900, 900));
+
 
 
     @Test
     void checkInternetConnection() {
+
         Purchase purchase = new Purchase();
 
         // checks that the internet connection is false form start
@@ -26,11 +30,13 @@ class PurchaseTest {
     @Test
     void addPhoneLine() {
 
+        // Given parameters
         ArrayList<String> cellphones = new ArrayList<>();
-        Purchase purchase = new Purchase(false, 8, cellphones, 0);
+        Purchase purchase = new Purchase(false, 9, cellphones, 0);
+        // When maximum of phonelines is exceeded
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> purchase.addPhoneLine());
+        // Then exeption error is shown
         assertEquals("8 phone lines is the maximum", exception.getMessage());
-
 
     }
 
@@ -40,6 +46,13 @@ class PurchaseTest {
 
     @Test
     void addCellPhone() {
+
+        Purchase purchase = new Purchase();
+        int i;
+        for(i = 0; i<cellPhones.size(); i++) {
+            assertEquals(purchase.addCellPhone(cellPhones.get(i)), cellPhonePrices.get(i));
+            purchase.price = 0;
+        }
     }
 
     @Test
